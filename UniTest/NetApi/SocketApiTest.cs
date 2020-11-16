8194137c17bca4e3f98a419aa3b5d153e7381c3d
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-16 21:10:29 +0500

    Добавлен проект для тестов NetApi библиотеки

---
+++ UniTest/NetApi/SocketApiTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniTest.NetApi
+{
+    [TestClass]
+    public class SocketApiTest
+    {
+        [TestMethod]
+        public void CreateBase()
+        {
+            
+        }
+    }
+}
