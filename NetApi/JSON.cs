1e4853255cdd2508d0ffde036ae7d8adef7c5f63
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 15:39:37 +0500

    Добавлен класс и метод для сериалазации объекта

---
+++ NetApi/JSON.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace NetApi
+{
+    /// <summary>
+    /// Методы для сериализации и десериалазции объектов
+    /// </summary>
+    class JSON
+    {
+        /// <summary>
+        /// Сериализация объекта 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public byte[] Serialize(object obj)
+        {
+            string res = JsonSerializer.Serialize(obj);
+            return Encoding.Unicode.GetBytes(res);
+        }
+
+        /*
+        public object Deserialize(byte[] serializeObj)
+        {
+            string str = Encoding.Unicode.GetString(serializeObj);
+            return JsonSerializer.Deserialize(;
+        }*/
+    }
+}
