3c03cc521db4c35fa59f707baf859a72471106c8
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 12:49:36 +0500

    Добавление тествого проекта интерфейса

---
+++ TestInterface/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestInterface
+{
+
+    /// <summary>
+    /// Тестовый проект являющийся заглушкей приложения интерфейса
+    /// Используется для разработки части веб сервера приложения ConveyorApp
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
