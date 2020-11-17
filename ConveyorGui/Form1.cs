a62bc721896ab9232c25234e21d6c3917cb5e707
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 23:50:07 +0500

    Добавлен метод для заполнения панели продуктами

--- ConveyorGui/Form1.cs
+++ ConveyorGui/Form1.cs
@@ -15,6 +15,31 @@ namespace ConveyorGui
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        /// <summary>
+        /// Заполнение панели с элементами конвейера
+        /// </summary>
+        /// <param name="arr"></param>
+        void FillingPanel(int[] arr)
+        {
+            //Удаление всех элементов
+            ConveyorPanel.Controls.Clear();
+            //Добавление новых
+            foreach(int Product in arr)
+            {
+                Panel product = new Panel();
+                product.Height = ConveyorPanel.Height - 2;
+                product.Width = 90;
+                //В зависимоти от типа - выбор цвета
+                if(Product == 1)
+                    product.BackColor = Color.Green;
+                if (Product == 2)
+                    product.BackColor = Color.Yellow;
+
+                ConveyorPanel.Controls.Add(product);
+            }
         }
 
     }
