b8080c791b169dcc9b9fdc1ebdb12fc6746d6f81
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 17:54:02 +0500

    Добавлен класс API для отправления запросов на сервер со стороны клиента

---
+++ TestInterface/ConveyorApi.cs
@@ -0,0 +1,27 @@
+﻿using NetApi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestInterface
+{
+    /// <summary>
+    /// Класс, предоставляющий интерфейс для работы с конвеером
+    /// </summary>
+    public class ConveyorApi
+    {
+        /// <summary>
+        /// СОкет для отправки сообщений
+        /// </summary>
+        SendSocket sendSocket = null;
+
+        /// <summary>
+        /// Создание новго АПИ для работы с конвеером
+        /// </summary>
+        /// <param name="SendSocket">Сокет, через который происходит связь с сервером</param>
+        public ConveyorApi(SendSocket SendSocket)
+        {
+
+        }
+    }
+}
