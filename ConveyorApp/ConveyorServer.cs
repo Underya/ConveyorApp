6caf50ab6553d23e63a6b7b00759ff1f7f64a369
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 13:19:25 +0500

    Добавлен класс сервера для прослушивания сообщений

---
+++ ConveyorApp/ConveyorServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetApi;
+
+namespace ConveyorApp
+{
+    /// <summary>
+    /// Класс для оработки и получения запросов по IP
+    /// </summary>
+    public class ConveyorServer
+    {
+
+        /// <summary>
+        /// Конвеер с продуктами
+        /// </summary>
+        IConveyor conveyor = null;
+
+        /// <summary>
+        /// Сокет, с помощью которого осуществляется прослушивание
+        /// </summary>
+        ListenSocket listenSocket = null;
+
+        /// <summary>
+        /// Создание нового сервера, работающего с указанным сервером
+        /// </summary>
+        /// <param name="Conveyor"></param>
+        public ConveyorServer(IConveyor Conveyor, ListenSocket ListenSocket)
+        {
+            ///Сохранение конвейера
+            conveyor = Conveyor;
+            //Созранение сокета
+            listenSocket = ListenSocket;
+            //Добавляется метод для прослушивания получаемых сообщений
+            listenSocket.AcceptMessage += AcceptMessage;
+        }
+
+        /// <summary>
+        /// Запуск прослушивания сервера 
+        /// </summary>
+        public void StartLisetn()
+        {
+            Console.WriteLine("Сервер запущен");
+            //Запуск прослушивания
+            listenSocket.StartListen();
+        }
+
+        /// <summary>
+        /// Обработка сообщений, получаемых сервером
+        /// </summary>
+        /// <param name="Messagem"></param>
+        /// <param name="SizeMessage"></param>
+        void AcceptMessage(byte []Messagem, int SizeMessage)
+        {
+            Console.WriteLine("Получено сообщение");
+            //Получение сообщения и его певерод в строку
+            string Calling = Encoding.Unicode.GetString(Messagem, 0, SizeMessage);
+            Console.WriteLine("Сообщение: {0}", Calling);
+        }
+
+    }
+}
