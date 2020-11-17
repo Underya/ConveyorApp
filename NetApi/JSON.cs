b59a06d2ce547a978b85031d6c9b9d7bfe74a6ef
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-11-17 17:35:58 +0500

    Добавлены и проведены тесты сериализации

--- NetApi/JSON.cs
+++ NetApi/JSON.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace NetApi
@@ -12,19 +13,35 @@ namespace NetApi
     public class JSON
     {
         /// <summary>
-        /// Сериализация объекта 
+        /// Сериализация объектов, int, int[], string, Exception
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
         public byte[] Serialize(object obj)
         {
-            string res = JsonSerializer.Serialize(obj, obj.GetType());
+            string res = "";
+            //Получение имени типа объета
+            string type = obj.GetType().Name;
+            //Если тип является исключением
+            if (type == "Exception")
+            {
+                //То сохраняет только сообщение
+                res = ((Exception)(obj)).Message;
+                //Тип указывается как строка
+                type = "Exception";
+                res = JsonSerializer.Serialize(res, res.GetType());
+            }
+            else 
+                //Сериализация объекта
+                res = JsonSerializer.Serialize(obj, obj.GetType());
+
             //К объекту приписывается его тип
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.Append("Type:");
-            stringBuilder.Append(obj.GetType().Name);
+            stringBuilder.Append(type);
             stringBuilder.Append(";");
             stringBuilder.Append(res);
+
             return Encoding.Unicode.GetBytes(stringBuilder.ToString());
         }
 
@@ -33,7 +50,7 @@ namespace NetApi
             string str = Encoding.Unicode.GetString(serializeObj);
             //Получение типа объекта
             string type = str.Substring(str.IndexOf(':') + 1, str.IndexOf(';') - str.IndexOf(':') -1);
-            //Получение объекта без типа
+            //Получение объекта без типа 
             string strObject = str.Substring(str.IndexOf(';')+1, str.Length - str.IndexOf(';')-1);
 
             //Десериализация в зависимоти от типа
@@ -41,7 +58,10 @@ namespace NetApi
                 return JsonSerializer.Deserialize<string>(strObject);
 
             if (type == "Exception")
-                return JsonSerializer.Deserialize<Exception>(strObject);
+            {
+                string Message = JsonSerializer.Deserialize<string>(strObject);
+                return new Exception(Message);
+            }
 
             if (type == "Int32[]")
                 return JsonSerializer.Deserialize<Int32[]>(strObject);
@@ -52,5 +72,6 @@ namespace NetApi
             //Если не шалось типа
             return JsonSerializer.Deserialize<object>(strObject);
         }
+
     }
 }
---
+++ UniTest/NetApi/JSONtests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NetApi;
+
+namespace UniTest.NetApi
+{
+    [TestClass]
+    public class JSONtests
+    {
+        string text = "Text for serialize; текст: сериализация";
+
+        int int_val = -123;
+
+        Exception exception = new Exception("JSON TEXT");
+
+        int[] int_arr_vall = new int[] { 1, 2, 3, 0, -5 };
+
+        [TestMethod]
+        public void Create()
+        {
+            JSON json = new JSON();
+        }
+
+        [TestMethod]
+        public void SerializeText()
+        {
+            JSON json = new JSON();
+            json.Serialize(text);
+        }
+
+        [TestMethod]
+        public void SerializeInt()
+        {
+            JSON json = new JSON();
+            json.Serialize(int_val);
+        }
+
+        [TestMethod]
+        public void SerializeIntArr()
+        {
+            JSON json = new JSON();
+            json.Serialize(int_arr_vall);
+        }
+
+        [TestMethod]
+        public void SerializeExcept()
+        {
+            JSON json = new JSON();
+            json.Serialize(exception);
+        }
+
+        [TestMethod]
+        public void SerializeSelfObj()
+        {
+            JSON json = new JSON();
+            json.Serialize(json);
+        }
+
+        [TestMethod]
+        public void DeSerializeTypeInt()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(int_val);
+            object int_value = json.Deserialize(result);
+            int type = 123;
+            Assert.AreEqual(type.GetType(), int_value.GetType());
+        }
+
+        [TestMethod]
+        public void DeSerializeValyeInt()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(int_val);
+            object int_value = json.Deserialize(result);
+            Assert.AreEqual(int_val, (int)int_value);
+        }
+
+        [TestMethod]
+        public void DeSerializeTypeString()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(text);
+            object string_value = json.Deserialize(result);
+            Assert.AreEqual(text.GetType(), string_value.GetType());
+        }
+
+        [TestMethod]
+        public void DeSerializeValueString()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(text);
+            object string_value = json.Deserialize(result);
+            Assert.AreEqual(text, (string)string_value);
+        }
+
+        [TestMethod]
+        public void DeSerializeTypeException()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(exception);
+            object exception_value = json.Deserialize(result);
+            Assert.AreEqual(exception.GetType(), exception_value.GetType());
+        }
+
+        [TestMethod]
+        public void DeSerializeValueException()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(exception);
+            Exception exception_value = (Exception)json.Deserialize(result);
+            Assert.AreEqual(exception.Message, exception_value.Message);
+        }
+
+        [TestMethod]
+        public void DeSerializeTypeIntArr()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(int_arr_vall);
+            int[] arr = (Int32[])json.Deserialize(result);
+            Assert.AreEqual(int_arr_vall.ToString(), arr.ToString());
+        }
+
+        [TestMethod]
+        public void DeSerializeValueIntArr()
+        {
+            JSON json = new JSON();
+            byte[] result = json.Serialize(int_arr_vall);
+            int[] arr = (Int32[])json.Deserialize(result);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(int_arr_vall[i], arr[i]);
+            }
+        }
+    }
+}
